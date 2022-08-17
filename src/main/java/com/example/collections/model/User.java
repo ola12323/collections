6eec196f3bbed5ad02f0daa1df3ba9710ae159f4
aleksandra <aleksandra.kacprowicz12@gmail.com>
2022-08-17 16:58:02 +0200

    change11

--- src/main/java/com/example/collections/model/User.java
+++ src/main/java/com/example/collections/model/User.java
@@ -35,7 +35,7 @@ import org.springframework.security.core.userdetails.UserDetails;
         public String getPhotosImagePath() {
             if (photos == null || id == null) return null;
 
-            return "/static/images/user-photos/" + id + "/" + photos;
+            return "/images/user-photos/" + id + "/" + photos;
         }
         public void setPhotos(String photos) {
             this.photos = photos;
