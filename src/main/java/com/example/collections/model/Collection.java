739f015b19220baa2d655b546ab4483d77f8cc37
aleksandra <aleksandra.kacprowicz12@gmail.com>
2022-08-17 15:11:33 +0200

    change5

---
+++ src/main/java/com/example/collections/model/Collection.java
@@ -0,0 +1,105 @@
+package com.example.collections.model;
+
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Set;
+
+@Entity
+public class Collection {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NotBlank(message = "nameErr")
+    private String name;
+    private String description;
+    private String theme;
+    @ManyToOne(fetch = FetchType.EAGER)
+    private User owner;
+    @OneToMany(mappedBy = "collection", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Item> items;
+    @Column(nullable = true, length = 64)
+    private String photos;
+
+    public String getPhotos() {
+        return photos;
+    }
+    @Transient
+    public String getPhotosImagePath() {
+        if (photos == null || id == null) return null;
+
+        return "/user-photos/" + id + "/" + photos;
+    }
+    public void setPhotos(String photos) {
+        this.photos = photos;
+    }
+    public Collection(String name, String description, String theme, User user, String photos) {
+        this.owner = user;
+        this.name = name;
+        this.description = description;
+        this.theme = theme;
+        this.photos=photos;
+    }
+
+    public Collection() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getHtmlDescription() {
+        Parser parser = Parser.builder().build();
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        Node document = parser.parse(description);
+        return new String(renderer.render(document));
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTheme() {
+        return theme;
+    }
+
+    public void setTheme(String theme) {
+        this.theme = theme;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public Set<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(Set<Item> items) {
+        this.items = items;
+    }
+}
---
+++ src/main/java/com/example/collections/model/Item.java
@@ -0,0 +1,117 @@
+package com.example.collections.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NotBlank(message = "nameErr")
+    private String name;
+    private String tag;
+    @ManyToOne
+    private Collection collection;
+    @ManyToMany
+    @JoinTable(
+            name = "item_tags",
+            joinColumns = @JoinColumn(name = "item_id"),
+            inverseJoinColumns = @JoinColumn(name = "tags_id")
+    )
+    private Set<Tag> tagSet;
+    @ManyToMany
+    @JoinTable(
+            name="item_likes",
+            joinColumns = @JoinColumn(name="item_id"),
+            inverseJoinColumns = @JoinColumn(name="user_id")
+    )
+    private Set<User> likes = new HashSet<>();
+    @OneToMany(mappedBy = "item", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Comment> comments = new HashSet<>();
+
+    public Item(String name, String tag, Collection collection) {
+        this.name = name;
+        this.tag = tag;
+        this.collection = collection;
+        this.tagSet = new HashSet<>();
+
+    }
+    @Column(nullable = true, length = 64)
+    private String photos;
+
+    public String getPhotos() {
+        return photos;
+    }
+    @Transient
+    public String getPhotosImagePath() {
+        if (photos == null || id == null) return null;
+
+        return "/user-photos/" + id + "/" + photos;
+    }
+    public void setPhotos(String photos) {
+        this.photos = photos;
+    }
+    public Item() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public Collection getCollection() {
+        return collection;
+    }
+
+    public void setCollection(Collection collection) {
+        this.collection = collection;
+    }
+
+    public Set<Tag> getTagSet() {
+        return tagSet;
+    }
+
+    public void setTagSet(Set<Tag> tagSet) {
+        this.tagSet = tagSet;
+    }
+
+    public Set<User> getLikes() {
+        return likes;
+    }
+
+    public void setLikes(Set<User> likes) {
+        this.likes = likes;
+    }
+
+    public Set<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(Set<Comment> comments) {
+        this.comments = comments;
+    }
+}
